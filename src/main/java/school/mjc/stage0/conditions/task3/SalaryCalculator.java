fe3cd0e8a875f18a82b52fbde62f7a11420c217e
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-06-23 15:14:10 +0300

    v2

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -4,12 +4,14 @@ public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if (salary < 0)
             System.out.println("wrong input!");
-        else if (salary <= 10000)
-            salary *= 0.85;
-        else if (salary <= 20000)
-            salary *= 0.82;
-        else
-            salary *= 0.8;
-        System.out.println(salary);
+        else {
+            if (salary <= 10000)
+                salary *= 0.85;
+            else if (salary <= 20000)
+                salary *= 0.82;
+            else
+                salary *= 0.8;
+            System.out.println(salary / 1.);
+        }
     }
 }
